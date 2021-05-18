d378bb78b65bf22e653b058be239f8e7fc05077c
ally-kirst-kellogg <ally.kirst@kellogg.northwestern.edu>
2021-05-18 10:23:16 -0500

    trying

--- netlify/functions/firebase.js
+++ netlify/functions/firebase.js
@@ -1,14 +1,16 @@
 const firebase = require("firebase/app")
 require("firebase/firestore")
 
-const firebaseConfig = {
-  apiKey: "AIzaSyCGd7OAVUT1u3Sf9uoSh_6l8iHg2qh_JJ4",
-  authDomain: "kiei-451-spring-2021.firebaseapp.com",
-  projectId: "kiei-451-spring-2021",
-  storageBucket: "kiei-451-spring-2021.appspot.com",
-  messagingSenderId: "902466001150",
-  appId: "1:902466001150:web:b5a42e97ab21026b706daf"
-} // replace
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyDnbKx06RRW_V-P_C4fk4udshQUwSHHQ3Q",
+    authDomain: "kiei-451-49dcd.firebaseapp.com",
+    projectId: "kiei-451-49dcd",
+    storageBucket: "kiei-451-49dcd.appspot.com",
+    messagingSenderId: "34852354007",
+    appId: "1:34852354007:web:01c4df0f988950f29bfd92"
+  }
+ // replace
 
 if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig)
